50fda9d2fd5d3b5ca18d062ad2557a324722a41d
Michael_McGrath <corecreate0@gmail.com>
2023-04-10 19:25:26 +0200

    firstcommit

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,28 @@
 import "./App.css";
 import "./app.scss";
+import anetra from "./img/anetra.png";
+import mistress from "./img/mistress.jpeg";
+import sasha from "./img/Sasha.jpeg";
 
 function App() {
   return (
     <div className="wrapper">
       <Card
-        img=".src/img/anetra.png"
+        img={anetra}
         name="Anetra"
         description="'Anetra' was the name of a stripper that she used to work with - she
           chose the name because Anetra was 'the hottest thing she ever saw in
           her life'."
       />
       <Card
-        img="https://static.wikia.nocookie.net/logosrupaulsdragrace/images/4/42/Anetra.jpeg/revision/latest?cb=20221213163345"
+        img={mistress}
         alt="drag queen"
         name="Mistress"
         description=" 'Mistress' is a double entendre for 'a bitch who is in control' and 'a sexy slut'. 'Isabelle' is the feminine version of her real name, Israel. 
         'Brooks' comes for her drag mother, Chevelle Brooks."
       />
       <Card
-        img="https://static.wikia.nocookie.net/logosrupaulsdragrace/images/8/84/SashaColby.jpeg/revision/latest/scale-to-width-down/350?cb=20221213165003"
+        img={sasha}
         alt="drag queen"
         name="Sasha Colby"
         description="'Sasha' was picked during her transitioning process because it is a gender-neutral name. 
@@ -28,12 +31,13 @@ function App() {
     </div>
   );
 }
+
 function Card(props) {
   return (
     <>
       <div className="card">
         <div className="card_body"></div>
-        <img src={props.img} alt="drag queen" />
+        <img className="card_image" src={props.img} alt="drag queen" />
         <h2 className="card_title">{props.name}</h2>
         <p className="card_desc">{props.description}</p>
         <button className="card_btn">View More</button>
